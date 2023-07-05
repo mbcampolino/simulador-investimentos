3175864fc17cfbe6042ed479e31cd6b0847380d0
Marcos <mbcampolino@gmail.com>
2023-07-05 00:45:53 -0300

    adiciona base das duas features do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimuladorRendaFixaComponent } from './simulador-renda-fixa/simulador-renda-fixa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimuladorRendaFixaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/module/results-fixed-investments/results-fixed-investments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results-fixed-investments',
+  templateUrl: './results-fixed-investments.component.html',
+  styleUrls: ['./results-fixed-investments.component.scss']
+})
+export class ResultsFixedInvestmentsComponent {
+
+}
