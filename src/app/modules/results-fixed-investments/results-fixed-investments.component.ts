717969acba213d5aadc0ce6b96c9ee662df176be
Marcos <mbcampolino@gmail.com>
2023-07-14 12:19:22 -0300

    add

--- src/app/modules/results-fixed-investments/results-fixed-investments.component.ts
+++ src/app/modules/results-fixed-investments/results-fixed-investments.component.ts
@@ -8,13 +8,15 @@ import {
   ApexChart,
   ApexXAxis,
   ApexTitleSubtitle,
-  ApexOptions
+  ApexOptions,
+  ApexYAxis
 } from "ng-apexcharts";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries | any;
   chart: ApexChart | any;
   xaxis: ApexXAxis | any;
+  yaxis: ApexYAxis | any;
   title: ApexTitleSubtitle | any;
   colors: ApexOptions | any;
 };
@@ -36,6 +38,8 @@ export class ResultsFixedInvestmentsComponent {
   }
 
   plotChart() {
+
+    /// faz calculo com observador, subscriber nesse componente e plota o grafico e a tabela
 
     var meses: number[] = []
     var valueTotal: number[] = []
@@ -70,6 +74,19 @@ export class ResultsFixedInvestmentsComponent {
       },
       xaxis: {
         categories: meses
+      },
+      yaxis: {
+        labels: {
+          formatter: function (n : number) {
+
+            if (n < 1e3) return n;
+            if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+            if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+            if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+            if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+            return n
+          }
+        },
       }
     };
 
