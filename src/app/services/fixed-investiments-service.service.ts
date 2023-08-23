ed279453b7c46214c7a48f8a9202fbdd4ae41597
Marcos <mbcampolino@gmail.com>
2023-08-22 23:47:16 -0300

    add files

--- src/app/services/fixed-investiments-service.service.ts
+++ src/app/services/fixed-investiments-service.service.ts
@@ -22,7 +22,7 @@ export class FixedInvestimentsServiceService {
     var backend = "/api/servico/sitebcb/historicotaxasjuros"
     var mock = "./../../assets/mock/mockselic.json"
 
-    this.getApiSelic(mock)
+    this.getApiSelic(backend)
   }
 
   // taxesByMonth() {
